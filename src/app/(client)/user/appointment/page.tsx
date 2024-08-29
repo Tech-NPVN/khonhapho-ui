6c9a89b70b04117f44bcb1b6ccf244b01aecb173
bou2201 <lamchuc123456@gmail.com>
2024-08-29 15:17:53 +0700

    feat(pages): :sparkles: handle ui user appointment

--- src/app/(client)/user/appointment/page.tsx
+++ src/app/(client)/user/appointment/page.tsx
@@ -1,5 +1,16 @@
+import Loading from '@/app/loading';
+import dynamic from 'next/dynamic';
+
+const UserAppointmentDynamic = dynamic(
+  () => import('@/modules/client/user/appointment').then((res) => res.UserAppointmentIndex),
+  {
+    ssr: false,
+    loading: () => <Loading />,
+  },
+);
+
 const AppointmentPage = () => {
-  return <></>;
+  return <UserAppointmentDynamic />;
 };
 
 export default AppointmentPage;
